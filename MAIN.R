d3c5d5014cfc3c5b04c16f4304389da186dea6bc
Sherman <saline@ucsd.edu>
2019-11-18 10:27:45 +0100

    merged sherman and Adriend work, changed variables to be the same in Q3

--- MAIN.R
+++ MAIN.R
@@ -71,10 +71,12 @@ bootimp_list[[j]] <- boot_temp$imp
 
 
 #QUESTION 3 #######
-library(MissSDA)
 # PCA methods
 # PCA has one objective
 #1. Summary a dataset (whith a lot of variable) by a new dataset with less variables (uncorelated between them) and keep all the information from the initial data set
+
+cor((amputed_list[[1]])[,-c(4)]) # NEED TO PUT INSIDE A LOOP
+# PCA is legitimate because variables are corelated
 
 library(FactoMineR)
 resuacp=PCA(amputed_list[[1]])
@@ -82,36 +84,21 @@ resuacp$eig
 tabeigenvalue=resuacp$eig[,1]
 plot(tabeigenvalue)
 lines(tabeigenvalue)
-
-
 # We have 3 criterias
 # 1. The cumulative percentage of variance must be greater than 70%, with this criteria we're going to take 2 components
 # 2. The eigen-value greater than 1, so we will take 2 components
 # 3. When the slope of eigenvalue graph change a lot, so with this criteria we will take 2 components.
 
-
-
-#regularized , iterative PCA
-
-#This work is based on the slides.
-cor(base3[,-c(4)])
+result=PCA(amputed_list[[1]],ncp=2)
+result$var
+result$var$contrib
+# threshold=100/number of variable in the first dataset = 100/3 = 33,3%
+# The variables which contribuate to the component 1 are waget and educ
+# Only exper contribuates to the component 2
+# We can see also the component 1 is strongly correlated with waget and educ and the component 2 with exper
 
 
 #for(j in 1:M){
 res.MIPCA <- MIPCA(amputed_list[[1]], ncp = 2, nboot  = B, method="Regularized") #note that nb$ncp = 0
 res.MIPCA$res.MI
 #}
-
-
-res.comp$completeObs[1:3, ]
-
-
-
-
-
-
-#removed FactoMineR because instructed to use Miss SDA
-imp1df2pca <- PCA(df2, ncp = nb$ncp) #this pca method only does mean imputation.
-
-
-plot.PCA(imp1df2pca)
